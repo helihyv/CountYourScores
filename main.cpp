684ea07f18d825c261b0c8f2b469c4eade225e4b
Heli Hyvättinen <heli.hyvattinen@kymp.net>
2012-08-03 18:41:26 +0300

    UI shows up
    Now registers the C++ model *before* creating the UI.

--- main.cpp
+++ main.cpp
@@ -2,19 +2,17 @@
 #include "qmlapplicationviewer.h"
 #include <QtDeclarative>
 #include "replaceableintegerlistmodel.h"
+//#include <QDebug>
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 
+   qmlRegisterType<ReplaceableIntegerListModel> ("countyourscores",0,1,"ReplaceableIntegerListModel");
 
     QmlApplicationViewer viewer;
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer.setMainQmlFile(QLatin1String("qml/CountYourScores/main.qml"));
-
-    qmlRegisterType<ReplaceableIntegerListModel> ("countyourscores",0,1,"ReplaceableIntegerListModel");
-
-
     viewer.showExpanded();
 
     return app->exec();
