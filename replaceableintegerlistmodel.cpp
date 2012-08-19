3799aadbda936e7829cea76030a67065196c0de9
Heli Hyvättinen <heli.hyvattinen@kymp.net>
2012-08-19 17:13:47 +0300

    Added copyright/license info (cherry picked from commit 120fb08f00e12aa1ef82fe151399f729efc686fc)

--- replaceableintegerlistmodel.cpp
+++ replaceableintegerlistmodel.cpp
@@ -1,3 +1,23 @@
+
+/**************************************************************************
+**  replaceableintegerlistmodel.cpp of CountYourScores
+**  Copyright (c) 2012 Heli Hyvättinen
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  See <http://www.gnu.org/licenses/>
+**
+**  ReplaceableIntegerListModel 19.8.2012
+**************************************************************************/
+
 #include "replaceableintegerlistmodel.h"
 
 ReplaceableIntegerListModel::ReplaceableIntegerListModel(QObject *parent) :
